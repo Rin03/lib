58ec29a3b41394e678db80420967c00491b64c1a
Rin03 <rindertvdhoeven@gmail.com>
2024-09-01 16:32:02 +0200

    Update thememanager.lua

--- thememanager.lua
+++ thememanager.lua
@@ -6,7 +6,7 @@ local ThemeManager = {} do
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
 		["default"] 		= { 1, httpService:JSONDecode("{"FontColor":"ffffff","MainColor":"1c1c1c","AccentColor":"ced83c","BackgroundColor":"141414","OutlineColor":"323232"}") },
-		["barbie"] 			= {2, httpsService:JSONDecode("{"FontColor":"8eccfe","MainColor":"fbc6fb","AccentColor":"f9f0a4","BackgroundColor":"f5f7c3","OutlineColor":"ffffff"}") },
+		["barbie"] 			= {2, httpService:JSONDecode("{"FontColor":"8eccfe","MainColor":"fbc6fb","AccentColor":"f9f0a4","BackgroundColor":"f5f7c3","OutlineColor":"ffffff"}") },
 	}
 
 	function ThemeManager:ApplyTheme(theme)
