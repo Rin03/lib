02384885dc402ed9bd733f3cfccb2d9445fa9126
Rin03 <rindertvdhoeven@gmail.com>
2025-01-12 15:27:48 +0100

    idk

--- thememanager.lua
+++ thememanager.lua
@@ -147,7 +147,7 @@ local ThemeManager = {} do
 
 		Options.ThemeManager_CustomThemeList:OnChanged(function()
 			if Options.ThemeManager_CustomThemeList.Value == nil then
-				pcall(Options.ThemeManager_CustomThemeList.Value:SetValue("default.json"))
+				pcall(Options.ThemeManager_CustomThemeList:SetValue("default.json"))
 			else
 				self:ApplyTheme(Options.ThemeManager_CustomThemeList.Value)
 			end
